cde79f02e0a0584b709882f29fe74f4cf1f7a334
MorozovGeeK <morozov4811@gmail.com>
2021-07-21 00:43:30 +0400

    rework-1

---
+++ src/main/resources/db/migration/data_store.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `document` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `series` bigint DEFAULT NULL,
+  `number` bigint DEFAULT NULL,
+  `document_date` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_type_idx` (`type_id`),
+  KEY `fk_user_idx` (`user_id`) /*!80000 INVISIBLE */,
+  CONSTRAINT `fk_type_id` FOREIGN KEY (`type_id`) REFERENCES `document_type` (`id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vi_0900_ai_ci;
+
+CREATE TABLE `document_type` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `document_name` varchar(45) COLLATE utf8mb4_vi_0900_ai_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_type_idx` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vi_0900_ai_ci;
+
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) COLLATE utf8mb4_vi_0900_ai_ci NOT NULL,
+  `last_name` varchar(45) COLLATE utf8mb4_vi_0900_ai_ci NOT NULL,
+  `patronymic` varchar(45) COLLATE utf8mb4_vi_0900_ai_ci DEFAULT NULL,
+  `birthday` date NOT NULL,
+  `sex` varchar(45) COLLATE utf8mb4_vi_0900_ai_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vi_0900_ai_ci;
